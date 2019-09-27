169010c18e8374e9714660d4e6312d378783e6c9
MrAtkens <r.kaliaskar@mail.ru>
2019-09-27 16:21:31 +0600

    AdminPanelTest

---
+++ build/precache-manifest.62cd3b88d005af3cec7086e68a9bea6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5a73e66d33780e62ae30143d2ca581e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c30231a4ebbb950f61bb",
+    "url": "/static/css/2.f40164c9.chunk.css"
+  },
+  {
+    "revision": "388814a6a1f9480111b2",
+    "url": "/static/css/main.1f03d4c9.chunk.css"
+  },
+  {
+    "revision": "c30231a4ebbb950f61bb",
+    "url": "/static/js/2.d1874a75.chunk.js"
+  },
+  {
+    "revision": "388814a6a1f9480111b2",
+    "url": "/static/js/main.5e2e4644.chunk.js"
+  },
+  {
+    "revision": "aa3aa33f924dbe75525d",
+    "url": "/static/js/runtime~main.5abdb152.js"
+  }
+]);
